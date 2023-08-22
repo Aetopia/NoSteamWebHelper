4be05ef5b3f2adc623b90926ce93307ee3349413
Aetopia <kirbymario22@gmail.com>
2023-08-22 11:41:56 +0530

    add void for unhandled error checking

--- src/DllMain.c
+++ src/DllMain.c
@@ -15,50 +15,50 @@ DWORD ThreadProc(LPVOID lpParameter)
     BOOL bRunning = FALSE;
     DWORD dwIndex = 0, cbData = 0;
 
-    RegOpenKeyExW(HKEY_CURRENT_USER, L"SOFTWARE\\Valve\\Steam\\Apps", 0, KEY_ALL_ACCESS, &hKey);
+    (VOID) RegOpenKeyExW(HKEY_CURRENT_USER, L"SOFTWARE\\Valve\\Steam\\Apps", 0, KEY_ALL_ACCESS, &hKey);
     do
     {
-        RegNotifyChangeKeyValue(hKey, TRUE, REG_NOTIFY_CHANGE_LAST_SET, hEvent, TRUE);
-        WaitForSingleObject(hEvent, INFINITE);
+        (VOID) RegNotifyChangeKeyValue(hKey, TRUE, REG_NOTIFY_CHANGE_LAST_SET, hEvent, TRUE);
+        (VOID) WaitForSingleObject(hEvent, INFINITE);
         while (!RegEnumKeyExW(hKey, dwIndex, rgwcName, &((DWORD){255}), NULL, NULL, NULL, NULL))
         {
-            RegOpenKeyExW(hKey, rgwcName, 0, KEY_ALL_ACCESS, &hSubKey);
-            RegGetValueW(hSubKey, NULL, L"Name", RRF_RT_REG_SZ, NULL, NULL, &cbData);
+            (VOID) RegOpenKeyExW(hKey, rgwcName, 0, KEY_ALL_ACCESS, &hSubKey);
+            (VOID) RegGetValueW(hSubKey, NULL, L"Name", RRF_RT_REG_SZ, NULL, NULL, &cbData);
             if (cbData)
             {
                 LPWSTR lpData = (LPWSTR)_malloca(cbData);
-                RegGetValueW(hSubKey, NULL, L"Name", RRF_RT_REG_SZ, NULL, lpData, &cbData);
-                RegGetValueW(hSubKey, NULL, L"Running", RRF_RT_REG_DWORD, NULL, &bRunning, &((DWORD){sizeof(BOOL)}));
+                (VOID) RegGetValueW(hSubKey, NULL, L"Name", RRF_RT_REG_SZ, NULL, lpData, &cbData);
+                (VOID) RegGetValueW(hSubKey, NULL, L"Running", RRF_RT_REG_DWORD, NULL, &bRunning, &((DWORD){sizeof(BOOL)}));
                 if (bRunning && !wcslen(rgwcData))
                 {
-                    wcscpy(rgwcData, lpData);
-                    SuspendThread((hThread = OpenThread(THREAD_SUSPEND_RESUME, FALSE,
-                                                        GetWindowThreadProcessId(
-                                                            FindWindowW(L"vguiPopupWindow", L"Untitled"),
-                                                            NULL))));
-                    CloseHandle(hThread);
+                    (VOID) wcscpy(rgwcData, lpData);
+                    (VOID) SuspendThread((hThread = OpenThread(THREAD_SUSPEND_RESUME, FALSE,
+                                                               GetWindowThreadProcessId(
+                                                                   FindWindowW(L"vguiPopupWindow", L"Untitled"),
+                                                                   NULL))));
+                    (VOID) CloseHandle(hThread);
                     if (Process32FirstW((hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0)), &pe))
                         do
                         {
                             if (!wcscmp(L"steamwebhelper.exe", _wcslwr(pe.szExeFile)))
                             {
-                                TerminateProcess((hProcess = OpenProcess(PROCESS_TERMINATE, FALSE, pe.th32ProcessID)), 0);
-                                CloseHandle(hThread);
+                                (VOID) TerminateProcess((hProcess = OpenProcess(PROCESS_TERMINATE, FALSE, pe.th32ProcessID)), 0);
+                                (VOID) CloseHandle(hThread);
                             }
                         } while (Process32NextW(hSnapshot, &pe));
-                    CloseHandle(hSnapshot);
+                    (VOID) CloseHandle(hSnapshot);
                 }
                 else if (!bRunning && !wcscmp(lpData, rgwcData))
                 {
-                    memset(rgwcData, 0, sizeof(rgwcData));
-                    ResumeThread((hThread = OpenThread(THREAD_SUSPEND_RESUME, FALSE,
-                                                       GetWindowThreadProcessId(
-                                                           FindWindowW(L"vguiPopupWindow", L"Untitled"),
-                                                           NULL))));
-                    CloseHandle(hThread);
+                    (VOID) memset(rgwcData, 0, sizeof(rgwcData));
+                    (VOID) ResumeThread((hThread = OpenThread(THREAD_SUSPEND_RESUME, FALSE,
+                                                              GetWindowThreadProcessId(
+                                                                  FindWindowW(L"vguiPopupWindow", L"Untitled"),
+                                                                  NULL))));
+                    (VOID) CloseHandle(hThread);
                 }
             }
-            RegCloseKey(hSubKey);
+            (VOID) RegCloseKey(hSubKey);
             dwIndex += 1;
         }
         dwIndex = 0;
@@ -70,8 +70,6 @@ DWORD ThreadProc(LPVOID lpParameter)
 BOOL WINAPI DllMain(HINSTANCE hinstDll, DWORD fdwReason, LPVOID lpvReserved)
 {
     if (fdwReason == DLL_PROCESS_ATTACH)
-    {
-            CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)ThreadProc, NULL, 0, NULL);
-    }
+        (VOID) CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)ThreadProc, NULL, 0, NULL);
     return TRUE;
 }
--- src/WinMain.c
+++ src/WinMain.c
@@ -11,15 +11,15 @@ INT WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
                              .nShow = SW_SHOWNORMAL};
     DWORD cbData = 0, dwSize = sizeof(WCHAR) * (wcslen(L"NoSteamWebHelper.dll") + 1);
 
-    RegGetValueW(HKEY_CURRENT_USER, L"SOFTWARE\\Valve\\Steam", L"SteamPath", RRF_RT_REG_SZ, NULL, NULL, &cbData);
+  (VOID)  RegGetValueW(HKEY_CURRENT_USER, L"SOFTWARE\\Valve\\Steam", L"SteamPath", RRF_RT_REG_SZ, NULL, NULL, &cbData);
     sei.lpDirectory = (LPCWSTR)_malloca(cbData);
-    RegGetValueW(HKEY_CURRENT_USER, L"SOFTWARE\\Valve\\Steam", L"SteamPath", RRF_RT_REG_SZ, NULL, (PVOID)sei.lpDirectory, &cbData);
-    SetCurrentDirectoryW(sei.lpDirectory);
+  (VOID)   RegGetValueW(HKEY_CURRENT_USER, L"SOFTWARE\\Valve\\Steam", L"SteamPath", RRF_RT_REG_SZ, NULL, (PVOID)sei.lpDirectory, &cbData);
+  (VOID)   SetCurrentDirectoryW(sei.lpDirectory);
 
-    ShellExecuteExW(&sei);
+  (VOID)   ShellExecuteExW(&sei);
     lpBaseAddress = VirtualAllocEx(sei.hProcess, NULL, dwSize, MEM_COMMIT, PAGE_READWRITE);
-    WriteProcessMemory(sei.hProcess, lpBaseAddress, L"NoSteamWebHelper.dll", dwSize, NULL);
-    CloseHandle(CreateRemoteThread(sei.hProcess, 0, 0, (LPTHREAD_START_ROUTINE)LoadLibraryW, lpBaseAddress, 0, NULL));
-    CloseHandle(sei.hProcess);
+ (VOID)    WriteProcessMemory(sei.hProcess, lpBaseAddress, L"NoSteamWebHelper.dll", dwSize, NULL);
+ (VOID)    CloseHandle(CreateRemoteThread(sei.hProcess, 0, 0, (LPTHREAD_START_ROUTINE)LoadLibraryW, lpBaseAddress, 0, NULL));
+  (VOID)   CloseHandle(sei.hProcess);
     return EXIT_SUCCESS;
 }

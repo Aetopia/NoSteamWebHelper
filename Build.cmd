f344d99c30b518653dc3dd7a2d22085bdda4b359
Aetopia <kirbymario22@gmail.com>
2024-03-18 15:07:46 +0530

    Disable GPUAccelWebViewsV3.

--- Build.cmd
+++ Build.cmd
@@ -1,5 +1,5 @@
 @echo off
 cd "%~dp0"
-i686-w64-mingw32-gcc.exe -s -nostdlib -mwindows WinMain.c -lKernel32 -o NoSteamWebHelper.exe
+i686-w64-mingw32-gcc.exe -s -nostdlib -mwindows WinMain.c -lKernel32 -lAdvapi32 -o NoSteamWebHelper.exe
 i686-w64-mingw32-gcc.exe -s -nostdlib -shared DllMain.c -lKernel32 -lUser32 -lAdvapi32 -o NoSteamWebHelper.dll
 upx --best --ultra-brute dpapi.dll>nul 2>&1
--- WinMain.c
+++ WinMain.c
@@ -17,11 +17,17 @@ int WinMainCRTStartup()
         }
 
     PROCESS_INFORMATION ProcessInformation = {};
-    CreateProcessW(NULL,
-                   lstrcpyW(HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, 122),
-                            L"steam.exe -silent -cef-single-process -cef-disable-breakpad"),
-                   NULL, NULL, FALSE, CREATE_SUSPENDED, NULL, NULL, &((STARTUPINFOW){}), &ProcessInformation);
-
+    if (CreateProcessW(NULL,
+                       lstrcpyW(HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, 122),
+                                L"steam.exe -silent -cef-single-process -cef-disable-breakpad"),
+                       NULL, NULL, FALSE, CREATE_SUSPENDED, NULL, NULL, &((STARTUPINFOW){}), &ProcessInformation))
+    {
+        HKEY hKey = NULL;
+        RegCreateKeyExW(HKEY_CURRENT_USER, L"SOFTWARE\\Valve\\Steam", 0, NULL, REG_OPTION_NON_VOLATILE,
+                        KEY_CREATE_SUB_KEY | KEY_SET_VALUE, NULL, &hKey, NULL);
+        RegSetValueExW(hKey, L"GPUAccelWebViewsV3", 0, REG_DWORD, (BYTE *)&((DWORD){}), sizeof(DWORD));
+        RegCloseKey(hKey);
+    }
     LPVOID lpBaseAddress = VirtualAllocEx(ProcessInformation.hProcess, NULL, dwSize = 42, MEM_COMMIT, PAGE_READWRITE);
     WriteProcessMemory(ProcessInformation.hProcess, lpBaseAddress, L"NoSteamWebHelper.dll", dwSize, NULL);
     CloseHandle(CreateRemoteThread(ProcessInformation.hProcess, NULL, 0, (LPTHREAD_START_ROUTINE)LoadLibraryW,
